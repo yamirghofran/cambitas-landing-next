d4aeefa5aed2c96d57fb01540090ddcf880644d3
Yousef Amirghofran <yamirghofran@gmail.com>
2023-11-11 19:18:37 +0100

    General Commit

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google'
 import clsx from 'clsx'
+import OGImage from '@/app/cambitas-og.png'
 
 import '@/styles/tailwind.css'
 
@@ -9,21 +10,51 @@ const inter = Inter({
   variable: '--font-inter',
 })
 
-export const metadata = {
-  title: {
-    template: '%s - Cambitas',
-    default: 'Cambitas - Construction Management with Sustainability in Mind',
-  },
-  description:
-    'Cambitas is a construction management platform that helps you optimize your construction projects with sustainability in mind.',
-}
-
 export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
       className={clsx('h-full bg-gray-50 antialiased', inter.variable)}
     >
+      <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        <title>Cambitas - Construction Managament with Sustainibility in Mind</title>
+        <meta name="title" content="Cambitas - Construction Managament with Sustainibility in Mind" />
+        <meta
+          name="description"
+          content="Cambitas is a construction management platform that helps you manage your construction projects with sustainibility in mind."
+        />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.yourwebsite.com/page" />
+        <meta property="og:title" content="Cambitas - Construction Managament with Sustainibility in Mind" />
+        <meta
+          property="og:description"
+          content="Cambitas is a construction management platform that helps you manage your construction projects with sustainibility in mind."
+        />
+        <meta
+          property="og:image"
+          content={OGImage}
+        />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:url"
+          content="https://www.yourwebsite.com/page"
+        />
+        <meta property="twitter:title" content="Cambitas - Construction Managament with Sustainibility in Mind" />
+        <meta
+          property="twitter:description"
+          content="Cambitas is a construction management platform that helps you manage your construction projects with sustainibility in mind."
+        />
+        <meta
+          property="twitter:image"
+          content={OGImage}
+        />
+      </head>
       <body className="flex h-full flex-col">
         <div className="flex min-h-full flex-col">{children}</div>
       </body>
